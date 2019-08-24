5e7cc46b70722f5976b9e50d53bef3f3687d8fde
Ali <ali@Aliasgars-MacBook-Air.local>
2019-08-24 12:25:26 -0400

    Incremental updates and size reduction

--- Mnemonic Phrase/mnemonic_phrase.js
+++ Mnemonic Phrase/mnemonic_phrase.js
@@ -13,109 +13,45 @@
 
 
 // Select 32 Random hex values. For this iteration we will use hard coded values.
-
 var crypto = require("crypto");
 var hex_value = crypto.randomBytes(16).toString('hex');
 
 // Convert the hex values into equivalent binary number.
-
 function hex2bin(hex){
-    return (parseInt(hex, 16).toString(2)).padStart(4, '0');
-}
-
+    return (parseInt(hex, 16).toString(2)).padStart(4, '0');}
 var original_binary_value = ""
 hex_value.split("").forEach(str => {
-  original_binary_value += hex2bin(str)
-})
+  original_binary_value += hex2bin(str)})
 
 //Calculate SHA-256
-
 var sha256 = require('js-sha256');
 var sha256_hex_value = sha256(hex_value);
 
 //Select first value SHA-256 and convert into binary
-
 var first_value = sha256_hex_value.charAt(0)
 var first_value_binary = hex2bin(first_value);
 
 //Append the binary value into original binary value.
-
 var appended_binary = original_binary_value.concat(first_value_binary);
 
-
-//Split the binary into groups of 11 and convert into decimal.
-
-var to_decimal = []; // Array to store all decimal values.
-
-var to_decimal_1 = appended_binary.substr(0, 11);
-to_decimal_1 = parseInt(to_decimal_1, 2);
-to_decimal.push(to_decimal_1);
-
-var to_decimal_2 = appended_binary.substr(11, 11);
-to_decimal_2 = parseInt(to_decimal_2, 2);
-to_decimal.push(to_decimal_2);
-
-var to_decimal_3 = appended_binary.substr(22, 11);
-to_decimal_3 = parseInt(to_decimal_3, 2);
-to_decimal.push(to_decimal_3);
-
-var to_decimal_4 = appended_binary.substr(33, 11);
-to_decimal_4 = parseInt(to_decimal_4, 2);
-to_decimal.push(to_decimal_4);
-
-var to_decimal_5 = appended_binary.substr(44, 11);
-to_decimal_5 = parseInt(to_decimal_5, 2);
-to_decimal.push(to_decimal_5);
-
-var to_decimal_6 = appended_binary.substr(55, 11);
-to_decimal_6 = parseInt(to_decimal_6, 2);
-to_decimal.push(to_decimal_6);
-
-var to_decimal_7 = appended_binary.substr(66, 11);
-to_decimal_7 = parseInt(to_decimal_7, 2);
-to_decimal.push(to_decimal_7);
-
-var to_decimal_8 = appended_binary.substr(77, 11);
-to_decimal_8 = parseInt(to_decimal_8, 2);
-to_decimal.push(to_decimal_8);
-
-var to_decimal_9 = appended_binary.substr(88, 11);
-to_decimal_9 = parseInt(to_decimal_9, 2);
-to_decimal.push(to_decimal_9);
-
-var to_decimal_10 = appended_binary.substr(99, 11);
-to_decimal_10 = parseInt(to_decimal_10, 2);
-to_decimal.push(to_decimal_10);
-
-var to_decimal_11 = appended_binary.substr(110, 11);
-to_decimal_11 = parseInt(to_decimal_11, 2);
-to_decimal.push(to_decimal_11);
-
-var to_decimal_12 = appended_binary.substr(121, 11);
-to_decimal_12 = parseInt(to_decimal_12, 2);
-to_decimal.push(to_decimal_12);
-
-
 //Read File from local directory.
-
 const fs = require('fs') // Reading file from local assets.
 var list = fs.readFileSync('assets/bip39-wordlist.txt', 'utf-8');
 var word_list = list.split("\n"); // Store the string file in an array.
 
-//Generating 12 word phrase.
+//Split the binary into 11 and convert into decimal and generating 12 word phrase.
+var appended_binary_string = appended_binary.toString(); // Convert array into string.
+var appended_binary_sub_string = [], to_decimal = [], mnemonic_phrase = [];
+var j=0;
 
-var mnemonic_phrase = [];
-var m;
-
-	for(var i=0;i<12;i++)
-	{
-	m = to_decimal[i];
-	mnemonic_phrase.push(word_list[m]);
-	}
-
-/*console.log("Word List is:")
 for(var i=0;i<12;i++)
 {
-console.log(mnemonic_phrase[i]);
-}*/
+	appended_binary_sub_string = appended_binary_string.substr(j,11);
+	j=j+11;
+	to_decimal.push(parseInt(appended_binary_sub_string,2));
+	mnemonic_phrase.push(word_list[to_decimal[i]]);
+	console.log(mnemonic_phrase[i]);
+}
+
+console.log(mnemonic_phrase.length)
 
